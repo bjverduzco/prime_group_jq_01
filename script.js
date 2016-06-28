01b8a9a9221d5358a8cb491f848a47fb5fa9a787
Connor O'Gara <ogara.connor@gmail.com>
2016-06-28 15:57:02 -0500

    add html and click function

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
 function randomNumber(min, max) {
 	return Math.floor(Math.random() * (1 + max - min) + min);
 }
+
+$(function() {
+	var numApple = 0;
+	var numOrange = 0;
+	var numBanana = 0;
+	var numGrape = 0;
+	var bankAmount = 100;
+
+	$('.list-of-fruit').on('click','.fruit', function() {
+		var tempFruit = $(this).children().attr('class');
+
+		switch(tempFruit) {
+			case 'apple':
+				numApple++;
+				$('.number-apples').html(numApple);
+				break;
+		  case 'orange':
+				numOrange++;
+				$('.number-oranges').html(numOrange);
+				break;
+			case 'banana':
+				numBanana++;
+				$('.number-bananas').html(numBanana);
+				break;
+			case 'grape':
+				numGrape++;
+				$('.number-grapes').html(numGrape);
+				break;
+		}
+
+		$('.cash-money').html(bankAmount);
+
+
+	});
+
+
+
+
+
+
+
+
+
+
+});
